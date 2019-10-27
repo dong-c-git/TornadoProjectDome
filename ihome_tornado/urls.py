b5c299518d72fb0ed24e03c9404c5fff150af7db
dong.c <dc111000@hotmail.com>
2019-10-27 15:05:06 +0800

    ihomeTornado-2019-10-26-base-commit

--- ihome_tornado/urls.py
+++ ihome_tornado/urls.py
@@ -1,1 +1,9 @@
 #coding:utf-8
+import os
+from handlers.BaseHandler import StaticFileBaseHandler
+from handlers import Passport
+urls = [
+    (r'/api/register',Passport.RegisterHandler),
+    StaticFileBaseHandler,
+    dict(path=os.path.join(os.path.dirname(__file__),"html"),default_filename="index.html")
+]
